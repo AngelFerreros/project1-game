f37666ea51fdd57d494903e322c7d10d8ed07bf4
Angel Ferreros <AngelFerreros@tjs-macbook-pro.lan>
2019-12-23 15:31:57 +0800

    Re-factored startGame function and added more placeholders

--- game-script.js
+++ game-script.js
@@ -1,43 +1,55 @@
+console.log("js working");
 //start page has instructions button and play button
     // if play button is clicked, get user name then go to game page and start game
         //remove display of starter page and display the game page
 
 var playGameBtn = document.querySelector("#play");
-var instructBtn = document.querySelector("#instruct");
-var player;
+var player; //store name for msg later
 var score = 0;
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var startGame = false;
+var answer = []; //array for player answer
+var gameStart = false;
+
+
+//function to display gamepg
+var displayGame = function(){
+    var container = document.querySelector(".container"); // to append gamepg
+    var img = document.createElement("img");
+    img.classList.add("img-fluid");
+    img.src = movieQns[0].img
+    container.appendChild(img);
+debugger;
+    var buttons = document.createElement("div");
+    var letters = document.createElement("ul");
+        for (var i = 0; i < alphabet.length; i++) {
+        var list = document.createElement("li");
+        list.id = "letter";
+        list.innerHTML = alphabet[i]; //!alphabet not displaying!
+        container.appendChild(buttons);
+        container.appendChild(letters);
+        }
+};
+
+//function to hide main page and callback displayGame func
 var startGame = function(event){
 //get input value(name) for use in msg
-var nameField = document.querySelector("#input");
-var name = nameField.value;
-console.log(name);
+    var nameField = document.querySelector("#input");
+    var name = nameField.value;
+    console.log(name);
+    player = name;
 
-//display:none
-playGameBtn.classList.add("hide");
-instructBtn.classList.add("hide");
-nameField.classList.add("hide");
-console.log("change DOM");
+//hide main page elements - display:none
+    var starterField = document.getElementById("starter");
+    starterField.classList.add("hide");
+    console.log("hide buttons");
 
 // call function to display gamepage
+    displayGame();
+    gameStart = true;
 };
 //add click event listener on play btn to start game
 playGameBtn.addEventListener("click",startGame);
 console.log(playGameBtn);
-
-
-
-
-// if (gameStart == false) {
-//     //display main page
-//     //code block to addEventListener to start game
-// }
-
-
-//game pg (start game)
-
-// gameStart = true;
 
 
 
@@ -58,15 +70,12 @@ console.log(playGameBtn);
 
 
 // function checkAnswer() {
-//   var userAnswer = "How are you doing today?";
-//   var res = str.split(" ");
-//   document.getElementById("demo").innerHTML = res;
-// }
 
     //else,
         //title array - apply class - red letters
         //move to next qstn
         //score --
+// }
 
     //option to click "end" to close game
 //last pg
@@ -74,12 +83,9 @@ console.log(playGameBtn);
         //msg depends on tiered score
     //re-start button
 
-//object for movie questions
-// var movieQns ={
-// one: [" "],
-// two:[]
-// three:
-// four:
-// five:
-// };
-// console.log(movieQns.one);
+//sample array for movie questions (object)
+var movieQns = [
+{ question: 1, title: "Fast and Furious", img:"https://cdn3.whatculture.com/images/2019/02/a386635853d7653b-600x338.jpg"},
+{question: 2, title: "Star Wars", img: "https://static2.thequizimages.com/wordpress/wp-content/uploads/2018/03/movie-question-18-e1522270497513.jpg?q=50&fit=crop&w=963&h=481&dpr=1.5"}
+];
+console.log(movieQns[0].img);
